9d0b9e97d78ca23e896a3ce954f5e1af67580a7c
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2019-07-13 19:18:16 +0530

    problem in alpha code

--- 5_todo_with_cards/index.js
+++ 5_todo_with_cards/index.js
@@ -22,6 +22,28 @@ let saved_cards = [
                     striked: !false
                 }],
         edit_state : true
+    },    {
+        todos : [{
+                    title: 'code_saved',
+                    striked: false
+                },
+                {
+                    title: 'sleep_saved',
+                    striked: !false
+                }],
+        edit_state : false
+
+    },
+    {
+        todos : [{
+                    title: 'code_saved2',
+                    striked: false
+                },
+                {
+                    title: 'sleep_saved2',
+                    striked: !false
+                }],
+        edit_state : true
     },
 ] 
 let current_card = [{
@@ -37,30 +59,26 @@ let current_card = [{
 function render(current_card, saved_cards) {
 
     if(current_card.length != 0){
-        console.log('ff')
         var current_card_html =  current_card.reduce((acum, init,index) =>{
             return acum += `<li${init.striked? ` class = 'striked'`:''} data-id =${index} > ${init.title}</li>`
         }, '')
-        console.log(current_card_html)
     }else{
         var current_card_html = 'Enter Some Todos'
     }
 
     // showing saved cards
     if(saved_cards.length != 0){
-        var saved_cards_html= saved_cards.reduce((acum,init)=>{
-            return acum += print_card(init)
+        var saved_cards_html= saved_cards.reduce((acum,init,index)=>{
+            return acum += print_card(init,index)
         }, '')
     }else{
         var saved_cards_html = 'No Saved Card'
     }
 
-    console.log(current_card_html)
-
     return [current_card_html, saved_cards_html]
 
     // private function to render
-    function print_card(card){
+    function print_card(card, index){
         if (!card.edit_state){
             return `<div class = 's-card'>
                 <ul>
@@ -70,7 +88,7 @@ function render(current_card, saved_cards) {
                     },'')
                 }
                 </ul>
-                <button> Edit </button>
+                <button class='edit-btn' data-cardNo = ${index}> Edit </button>
             </div>`
         }else{
             return `<div class = 's-card'>
@@ -81,7 +99,7 @@ function render(current_card, saved_cards) {
                     },'')
                 }
                 </ul>
-                <button> Save Changes </button>
+                <button  class='savechanges-btn' data-cardNo = ${index} > Save Changes </button>
             </div>`
 
         }
@@ -92,8 +110,6 @@ function render(current_card, saved_cards) {
 // painting the html
 function paint(current_card, saved_cards){
     let cards_html = render(current_card,saved_cards)
-    console.log(cards_html)
-
     document.getElementById('todo-list').innerHTML = cards_html[0]
     document.getElementById('saved-cards').innerHTML = cards_html[1]
 }
@@ -130,6 +146,13 @@ document.getElementById('save-btn').addEventListener('click', (e) => {
     saved_cards = [...saved_cards , {todos: current_card, edit_state : false}] 
     current_card = [];
     paint(current_card, saved_cards)
-})
+});
+
+// moving to saving the card functionality
+let alpha  = document.getElementsByClassName('edit-btn');
+console.log(alpha)
+console.log('alpha length in vscode:  ',  alpha.length)
+
+
 // test
 paint(current_card, saved_cards)
