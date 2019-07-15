9418e2119c6e4be6efa01679b00681cad500dd1b
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2019-07-15 18:12:45 +0530

    solved Problems and Completed todo with save-cards feature

--- 5_todo_with_cards/index.js
+++ 5_todo_with_cards/index.js
@@ -1,158 +1,125 @@
 // states that defines our complete todo-list
-let saved_cards = [
-    {
-        todos : [{
-                    title: 'code_saved',
-                    striked: false
-                },
-                {
-                    title: 'sleep_saved',
-                    striked: !false
-                }],
-        edit_state : false
-
-    },
-    {
-        todos : [{
-                    title: 'code_saved2',
-                    striked: false
-                },
-                {
-                    title: 'sleep_saved2',
-                    striked: !false
-                }],
-        edit_state : true
-    },    {
-        todos : [{
-                    title: 'code_saved',
-                    striked: false
-                },
-                {
-                    title: 'sleep_saved',
-                    striked: !false
-                }],
-        edit_state : false
-
-    },
-    {
-        todos : [{
-                    title: 'code_saved2',
-                    striked: false
-                },
-                {
-                    title: 'sleep_saved2',
-                    striked: !false
-                }],
-        edit_state : true
-    },
-] 
-let current_card = [{
-        title: 'code',
-        striked: false
-    },{
-        title: 'sleep',
-        striked: !false
-    },
-]
+let saved_cards = []
+let current_card =[]
 
 // function to render html for page
 function render(current_card, saved_cards) {
 
-    if(current_card.length != 0){
-        var current_card_html =  current_card.reduce((acum, init,index) =>{
-            return acum += `<li${init.striked? ` class = 'striked'`:''} data-id =${index} > ${init.title}</li>`
-        }, '')
-    }else{
-        var current_card_html = 'Enter Some Todos'
-    }
+    const current_card_html = current_card.reduce((acum, init, index) => acum +
+        `<li ${init.striked? `class = 'striked'`: ''} data-id =${index} > 
+            ${init.title}
+        </li>`
+    , '')
 
     // showing saved cards
-    if(saved_cards.length != 0){
-        var saved_cards_html= saved_cards.reduce((acum,init,index)=>{
-            return acum += print_card(init,index)
-        }, '')
-    }else{
-        var saved_cards_html = 'No Saved Card'
-    }
+    const saved_cards_html = saved_cards.length != 0 
+    ? saved_cards.reduce((acum, card, index) => acum + print_card(card,index), '') 
+    : 'No Saved Card'
 
     return [current_card_html, saved_cards_html]
 
-    // private function to render
-    function print_card(card, index){
-        if (!card.edit_state){
-            return `<div class = 's-card'>
+    //private function to render
+    function print_card(card, index) {
+        return card.edit_state ?`
+        <div class ='s-card'>
+            <ul>
+            ${
+                card.todos.reduce((acum, init) => acum + 
+                    `</li>
+                        <input value = ' ${init.title} '>
+                    </li>`
+                ,'')
+            }
+            </ul>
+            <button data-action = 'save' data-cardno = ${index} > Save Changes </button>
+        </div>
+        `:`
+        <div class = 's-card'>
                 <ul>
                 ${
-                    card.todos.reduce((acum, init) =>{
-                        return acum += `<li${init.striked? ` class = 'striked'`:''}> ${init.title}</li>`
-                    },'')
+                    card.todos.reduce((acum, init) => acum +
+                        `<li${init.striked? ` class = 'striked'`:''}>
+                            ${init.title}
+                        </li>`
+                    ,'')
                 }
                 </ul>
-                <button class='edit-btn' data-cardNo = ${index}> Edit </button>
-            </div>`
-        }else{
-            return `<div class = 's-card'>
-                <ul>
-                ${
-                    card.todos.reduce((acum, init) =>{
-                        return acum += `</li><input placeholder ='${init.title}'></li>`
-                    },'')
-                }
-                </ul>
-                <button  class='savechanges-btn' data-cardNo = ${index} > Save Changes </button>
-            </div>`
-
-        }
+                <button data-action = 'edit' data-cardno = ${index}> Edit </button>
+                <button data-action = 'delete' data-cardno = ${index}> Delete Card </button>
+        </div>`
     }
-
 }
 
-// painting the html
-function paint(current_card, saved_cards){
+function paint(){
     let cards_html = render(current_card,saved_cards)
     document.getElementById('todo-list').innerHTML = cards_html[0]
     document.getElementById('saved-cards').innerHTML = cards_html[1]
+    // console.log('Done HTML Rendering!!!!')
 }
 
-// add functionality
+// test
+// paint(current_card, saved_cards)
+
+// Add Button
 document.getElementById('add-btn').addEventListener('click', (e) => {
     const { value } = document.getElementById('add-todo');
-    if(value == '') return;
+    if(value == '') return ;
     current_card.push({
         title : value,
         striked : false
     })
     document.getElementById('add-todo').value = ''
-    paint(current_card,saved_cards)
-
+    paint()
 })
 
-// striked functinality
+// striked functionality
 document.getElementById('todo-list').addEventListener('click', (e) => {
     const elementIdtoBeDeleted = e.target.dataset.id
     current_card[elementIdtoBeDeleted].striked = !current_card[elementIdtoBeDeleted].striked
-    paint(current_card, saved_cards)
+    paint()
 })
 
-//delete functionality
+// delete striked TODOs functionality
 document.getElementById('delete-btn').addEventListener('click', (e) => {
     current_card = current_card.filter(todo => todo.striked == false)
-    paint(current_card, saved_cards)
+    paint()
 })
 
-// saving the current card to saved_card list
+// saving the current card to saved_card list - Assuming User will always delete striked if not they will not be added 
 document.getElementById('save-btn').addEventListener('click', (e) => {
     if (current_card.length == 0) return ;
-    saved_cards = [...saved_cards , {todos: current_card, edit_state : false}] 
+    saved_cards = [...saved_cards , {todos: current_card.filter(item => !item.striked), edit_state : false}] 
     current_card = [];
-    paint(current_card, saved_cards)
+    paint()
 });
 
-// moving to saving the card functionality
-let alpha  = document.getElementsByClassName('edit-btn');
-console.log(alpha)
-console.log('alpha length in vscode:  ',  alpha.length)
+// Moving to saving the card functionality
+document.getElementById('saved-cards').addEventListener('click', function(e) {
 
+    const whichCard = e.target.dataset.cardno;
+    const action = e.target.dataset.action;
 
-// test
-paint(current_card, saved_cards)
+    if (! action){ 
+        return;
+    }
+
+    switch (action) {
+        case 'delete':
+            // splice(start:number, deleteCount:number)
+            saved_cards.splice(whichCard,1)
+            break
+        case 'edit':
+            saved_cards[whichCard].edit_state = !saved_cards[whichCard].edit_state            
+            break
+        case 'save':
+            const changes = [...e.target.parentNode.querySelectorAll('input')]
+            saved_cards[whichCard].todos = saved_cards[whichCard].todos.map((item, index) => (
+                {
+                    title : changes[index].value,
+                    striked : item.striked  
+                }
+            ))
+            saved_cards[whichCard].edit_state = !saved_cards[whichCard].edit_state
+    }
+    paint()   
+})
